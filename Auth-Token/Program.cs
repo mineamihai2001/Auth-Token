116f6efa1082d0724934d6fe897e83265eea94db
Mihai Minea <mineamihai2001@gmail.com>
2022-06-21 18:37:45 +0300

    first commit

---
+++ Auth-Token/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Test
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
